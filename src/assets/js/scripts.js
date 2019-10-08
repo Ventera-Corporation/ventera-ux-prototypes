5456d3b28a18184e2242308e86f581c80f6fec79
Sara Neel <saraaneel@gmail.com>
2019-10-08 09:51:23 -0400

    Hospice PA user  test

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -11,3 +11,51 @@ function toggleArrow(collapseHeading) {
   var collapseArrow = "#" + collapseHeading + " .iq-icon";
   $(collapseArrow).toggleClass("iq-icon-down");
 }
+
+// go to next vertical tab
+function goToNextTab(currentTabParam, nextTabParam) {
+  var currentTab = document.getElementById(currentTabParam + 'Tab');
+  var currentTabContent = document.getElementById(currentTabParam);
+
+  var nextTab = document.getElementById(nextTabParam + 'Tab');
+  var nextTabContent = document.getElementById(nextTabParam);
+
+  currentTab.classList.remove('active');
+  currentTabContent.classList.remove('active');
+
+  nextTab.classList.add('active');
+  nextTabContent.classList.add('active');
+
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+}
+
+// conditional content for select dropdown
+function toggleSelectContent(dropdownParam, conditionalContentParam, dropdownValueParam) {
+  var dropdown = document.getElementById(dropdownParam);
+  var dropdownValue = dropdown.options[dropdown.selectedIndex].value;
+  var conditionalContent = document.getElementById(conditionalContentParam)
+
+  if (dropdownValue == dropdownValueParam) {
+    conditionalContent.setAttribute('style', 'display: block');
+  }
+  else {
+    conditionalContent.setAttribute('style', 'display: none');
+  }
+}
+
+// conditional content for radio buttons
+function toggleRadioContent(radioParam, conditionalContentParam, radioValueParam1, radioValueParam2) {
+  var radio = document.getElementsByName(radioParam)[0];
+  var radioValue = document.querySelector('input[name="' + radioParam + '"]:checked').value;
+  var conditionalContent = document.getElementById(conditionalContentParam)
+
+  if (radioValue == radioValueParam1) {
+    conditionalContent.setAttribute('style', 'display: block');
+  }
+  else if (radioValue == radioValueParam2) {
+    conditionalContent.setAttribute('style', 'display: block');
+  }
+  else {
+    conditionalContent.setAttribute('style', 'display: none');
+  }
+}
