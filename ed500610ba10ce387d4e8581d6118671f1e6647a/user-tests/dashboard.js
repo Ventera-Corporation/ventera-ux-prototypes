b076eef99941668ea86bb4f612ae9a91fa2c989a
Sara Neel <saraaneel@gmail.com>
2019-10-08 09:49:04 -0400

    Moving dashboard user test

--- ed500610ba10ce387d4e8581d6118671f1e6647a/user-tests/dashboard.js
+++
@@ -1,9 +0,0 @@
-// const toast = $(".toast");
-
-// setTimeout(function () {
-//   toast.addClass("show");
-// }, 5000);
-
-// setTimeout(function () {
-//   toast.removeClass("show");
-// }, 10000);
